2ac6b5b4a230e11354f7de36fd0f840c166a5032
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-05-17 21:22:11 +0000

    Update logo design and favicon for the app

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,7 @@ const NavBar = () => {
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
-            <img src={logo} alt="logo" height="90" />
+            <img src={logo} alt="logo" height="75" />
           </Navbar.Brand>
         </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
