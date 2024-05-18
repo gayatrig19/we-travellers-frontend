7724405a70fac1f3465975216dfadaa22fa0ab17
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-05-18 21:51:55 +0000

    Add basic structure for sign in form and wire up the routes

---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,44 @@
+
+import React from "react";
+
+import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+
+import { Link } from "react-router-dom";
+
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+
+function SignInForm() {
+  return (
+    <Row className={styles.Row}>
+      <Col className="my-auto p-0 p-md-2" md={6}>
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>sign in</h1>
+        </Container>
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signup">
+            Don't have an account? <span>Sign up now!</span>
+          </Link>
+        </Container>
+      </Col>
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
+      >
+        <Image
+          className={`${appStyles.FillerImage}`}
+          src={"https://res.cloudinary.com/dpzitpjjc/image/upload/v1716061050/signup_cover_image_lajebr.webp"}
+        />
+      </Col>
+    </Row>
+  );
+}
+
+export default SignInForm;
