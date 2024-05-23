c61e540c893c363cc42b9164d8c2f88753628688
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-05-23 22:09:15 +0000

    Update styles for nav bar icons

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -45,7 +45,7 @@ function SignInForm() {
     try {
       const { data } = await axios.post("/dj-rest-auth/login/", signInData);
       setCurrentUser(data.user);
-      history.goBack();
+      history.goBack();     
     } catch (err) {
       setErrors(err.response?.data);
     }
